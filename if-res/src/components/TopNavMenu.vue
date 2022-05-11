fc5d291248a3d420f0a891ad40459390beb90664
HDU_Nbsp <1808347356@qq.com>
2022-05-11 19:21:49 +0800

    创建项目

---
+++ if-res/src/components/TopNavMenu.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="topMenu"></div>
+</template>
+
+<script>
+export default {
+  name: 'TopNavMenu'
+}
+</script>
+
+<style scoped>
+
+</style>
--- if-res/src/main.js
+++ if-res/src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import less from 'less'
 
+Vue.use(ElementUI) // 引入element ui组件库
+Vue.use(less) // 引入less css拓展
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -11,5 +16,6 @@ new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  render: h => h(app)
 })
